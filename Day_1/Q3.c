c1052e1fe0d1b05bf5a8e432f7910378e6de5bab
Karan Kumar <karankumar11910121@gmail.com>
2026-06-01 23:37:59 +0530

    Day_1 uploaded

---
+++ Day_1/Q3.c
@@ -0,0 +1,20 @@
+// Q3. Write a program to Find factorial of a number.
+
+#include <stdio.h>
+int main() 
+{
+    int n, i;
+    int factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) 
+    {
+        factorial *= i;
+    }
+
+    printf("Factorial of %d = %d\n", n, factorial);
+
+    return 0;
+}
